b59acce8b6199e30f659a9b8cb306aa3cc155207
Evelyn Stender <evelyn@inploi.com>
2023-03-07 09:07:33 +0000

    twofer

---
+++ two-fer/lib/two_fer.dart
@@ -0,0 +1,7 @@
+// This also works
+// String twoFer([String name = 'you']) {
+//   return 'One for $name, one for me.';
+// }
+
+// can be inline
+String twoFer([String person = "you"]) => 'One for $person, one for me.';
